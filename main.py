14d515a5e4d97be216998c4f745a7391b6836993
MathewIsBack <oseghaleokononfua@gmail.com>
2025-12-30 23:25:14 +0000

    my working version of sending emails with data from a fetched api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import date
+from send_email import send_email
 
 api_key = "70b082610d984c5d8679351192d366b5"
 today = date.today()
@@ -9,6 +10,19 @@ url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=70b0
 # make a request 
 request = requests.get(url)
 content = request.json()
+
+email_body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    title = article["title"]
+    description = article["description"]
+
+    email_body += f"""
+
+Title: {title}
+Description: {description}
+
+--------------------------
+"""
+    
+send_email(email_body)
---
+++ send_email.py
@@ -0,0 +1,29 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "oseghaleokononfua@gmail.com"
+    password = "hdfm jcqd pivf envz"
+
+    receiver = "bryanokononfua@gmail.com"
+    # context = ssl.create_default_context()
+
+    # Create MIME email (UTF-8)
+    msg = MIMEText(message, "plain", "utf-8")
+    msg["Subject"] = "Tesla News Update"
+    msg["From"] = username
+    msg["To"] = receiver
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        # server.sendmail(username, receiver, message)
+        server.send_message(msg)
+        
+
+# send_email("Hello, how are you?")
